5bb3778a26d4b8d57868b071af276136044d6451
Jorge Escalante <jescalantegt@gmail.com>
2014-10-09 08:38:27 -0600

    Se agrego interacción al menu y backbonejs al proyecto

--- sitioWeb/app/urls.py
+++ sitioWeb/app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^header/', TemplateView.as_view(template_name="partials/header.html"))
 )

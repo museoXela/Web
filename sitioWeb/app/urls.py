472a4f7833f438aa86f7f233d02bbf74fdbbfd03
Jorge Escalante <jescalantegt@gmail.com>
2014-10-10 23:38:45 -0600

    Corrección de error y maquetado de la pagina 'Pieza'

--- sitioWeb/app/urls.py
+++ sitioWeb/app/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^pieza', TemplateView.as_view(template_name="pieza.html")),
     (r'^header/', TemplateView.as_view(template_name="partials/header.html"))
 )
